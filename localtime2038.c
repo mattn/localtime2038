360c7b3aa1666a7418a69be6532822a4826ba17c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-10-26 13:40:30 +0900

    fix issue at 2100

--- localtime2038.c
+++ localtime2038.c
@@ -17,6 +17,16 @@ localtime(const time_t *timer) {
   unsigned long tmr = *timer;
   int yadd = 0;
 
+  /* 1970 - 2101 */
+  if (tmr >= (60uL*60uL*24uL)*47847uL) {
+    yadd += 40;
+    tmr -= (60uL*60uL*24uL)*14609uL;
+  }
+  /* 1970 - 2100 */
+  if (tmr >= (60uL*60uL*24uL)*47482uL) {
+    yadd += 6;
+    tmr -= (60uL*60uL*24uL)*2191uL;
+  }
   while (tmr >= ((60uL*60uL*24uL)*24837uL)) {
     yadd  +=28;
     tmr -= (60uL*60uL*24uL)*10227uL;
